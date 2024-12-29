282e648a3301b747cdd5a7de0836bee181828706
patty 💜 <87675545+p4tt7@users.noreply.github.com>
2024-12-29 17:27:25 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program{
+    public static int MinimumDifference(int[] a){
+        int minimum = Minimum(a);
+        int sec_minimum = SecMinimum(a, minimum);  
+        int diference = sec_minimum-minimum;
+        return diference;
+    }
+
+    public static int SecMinimum(int[] a, int minimum){
+       
+        int sec_minimum = int.MaxValue;
+
+        for(int i=0; i<a.Length; i++){
+            if(a[i]>minimum && a[i]<sec_minimum){
+                sec_minimum = a[i];
+            }
+        }
+        return sec_minimum;
+    }
+
+    public static int Minimum(int[] a){
+
+        int minimum = int.MaxValue;
+
+        for(int i=0; i<a.Length;i++){
+            if(a[i]<minimum){
+                minimum = a[i];
+            }
+        }
+        return minimum;
+    }
+
+    public static void Main(){
+        int[] a = {17,98,3,95,5};
+        int result = MinimumDifference(a);
+        Console.WriteLine("La diferencia es: " + result);
+    }
+}
+
+
